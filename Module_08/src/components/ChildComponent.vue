eb5a97cd40413e8a0a449b8e61f4335ee79078b0
ashekgo <88641371+ashekgo@users.noreply.github.com>
2023-08-20 18:48:45 +0600

    Add files via upload

---
+++ Module_08/src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['customMessage', 'message', 'messageEmitted'])
+defineEmits(['update:messageEmitted'])
+</script>
+
+<template>
+
+    <div class="px-4 mx-auto text-center">
+
+        <p class="text-5xl py-5 text-black-900 font-bold antialiased">{{ message }}</p>
+
+        <button @click="$emit('update:messageEmitted',customMessage)" class="px-3 py-2 font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-3xl">Display Message</button>
+
+    </div>
+</template>
---
+++ Module_08/src/components/ParentComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref} from 'vue'
+import ChildComponent from './ChildComponent.vue';
+const message = 'Message From Parent'
+const customMessage = 'Life Is Beautiful, Believe That & Work Hard To Make It.'
+const messageEmitted = ref('')
+</script>
+
+<template>
+    <div class="">
+        
+        <ChildComponent
+        :message="message" 
+        :customMessage = "customMessage" 
+        v-model:messageEmitted="messageEmitted"></ChildComponent>
+
+        <p class="text-5xl py-5 text-center text-black-900 font-bold antialiased">{{ messageEmitted }}</p>
+
+    </div>
+</template>
